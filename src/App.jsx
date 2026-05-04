96ea6ba630c39344fbbd77f82cc8f06325c50d84
himanshish2 <himanshish2@outlook.com>
2026-05-04 12:56:53 +0530

    fixing double slash error

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import professional from "./templates/professional.html?raw";
 import { fillTemplate } from "./utils/fillTemplate";
 import { downloadPortfolio } from "./utils/downloadPortfolio";
 
-const BACKEND_URL = "https://hack-backend-zl1d.onrender.com/"; // change to Railway URL later
+const BACKEND_URL = "https://hack-backend-zl1d.onrender.com"; // change to Railway URL later
 
 function App() {
   const [file, setFile] = useState(null);
@@ -35,7 +35,7 @@ function App() {
       setLoading(true);
 
       const res = await axios.post(
-        `${BACKEND_URL}api/upload`,
+        `${BACKEND_URL}/api/upload`,
         formData
       );
 
